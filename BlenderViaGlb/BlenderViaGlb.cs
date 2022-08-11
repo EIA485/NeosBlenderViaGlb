8cf99dcb9c0b079bf50556a6af00b4aacea309bf
EIA485 <17285570+EIA485@users.noreply.github.com>
2022-08-11 08:44:25 -0700

    Add project files.

---
+++ BlenderViaGlb/BlenderViaGlb.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using NeosModLoader;
+using BaseX;
+using CodeX;
+namespace BlenderViaGlb
+{
+    public class BlenderViaGlb : NeosMod
+    {
+        public override string Name => "BlenderViaGlb";
+        public override string Author => "eia485";
+        public override string Version => "1.0.0";
+        public override string Link => "https://github.com/EIA485/NeosBlenderViaGlb/";
+
+        public override void OnEngineInit()
+        {
+            Harmony harmony = new Harmony("net.eia485.BlenderViaGlb");
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(BlenderInterface), "ExportToGLTF")]
+        private class BlenderViaGlbPatch
+        {
+            public static bool Prefix(string input, string output)
+            {
+                output = output.Replace("/", "\\").Replace("\\", "\\\\");
+                output = output.EscapeUnicodeCharacters();
+                typeof(BlenderInterface).GetMethod("RunScript").Invoke(null,new object[] { "import bpy\r\nbpy.ops.export_scene.glb(filepath=u\"" + output + "\")", "\"" + input + "\" --background --python \"{0}\"" });
+                return false;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# BlenderViaGlb
+
+A [NeosModLoader](https://github.com/zkxs/NeosModLoader) mod for [Neos VR](https://neos.com/) that changes blender exports to proxy via glb instead of gltf
+
+
+## Installation
+1. Install [NeosModLoader](https://github.com/zkxs/NeosModLoader).
+1. Place [BlenderViaGlb.dll](https://github.com/eia485/NeosBlenderViaGlb/releases/latest/download/BlenderViaGlb.dll) into your `nml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\NeosVR\nml_mods` for a default install. You can create it if it's missing, or if you launch the game once with NeosModLoader installed it will create the folder for you.
+1. Start the game. If you want to verify that the mod is working you can check your Neos logs.
